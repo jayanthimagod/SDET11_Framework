44c3d09a34cb0db20ce2780d0c34a501f548afbc
91948 <91948@DESKTOP-KR0BRDU>
2021-03-15 13:05:17 +0530

    commit to local repository

---
+++ GithubTrialFramework/src/test/java/com/TYSS/GithubTrialFramework/CreateContactTest.java
@@ -0,0 +1,18 @@
+package com.TYSS.GithubTrialFramework;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("execute create contact test");
+	}
+	public void modifycontacttest(){
+		System.out.println("execute modify contact test");
+	}
+	public void deletecontacttest(){
+		System.out.println("execute delete contact test");
+	}
+}
